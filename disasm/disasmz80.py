264252766a4a5ee1a57bbe0ab15cd006b0c32480
Jeff Tranter <tranter@pobox.com>
2013-09-30 12:54:25 -0400

    Start implementation of Z80 disassembler.

--- disasm/disasmz80.py
+++ disasm/disasmz80.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 #
-# Disassembler for Intel 8080 microprocessor.
+# Disassembler for Zilog Z80 microprocessor.
 # Copyright (c) 2013 by Jeff Tranter <tranter@pobox.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,140 +30,140 @@ signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 # Lookup table - given opcode byte as index, return mnemonic of instruction and length of instruction.
 lookupTable = [
   [ "nop", 1 ],         # 00
-  [ "lxi     b,", 3 ],  # 01
-  [ "stax    b", 1 ],   # 02
-  [ "inx     b", 1 ],   # 03
-  [ "inr     b", 1 ],   # 04
-  [ "dcr     b", 1 ],   # 05
-  [ "mvi     b,", 2 ],  # 06
-  [ "rlc",       1 ],   # 07
-  [ "*nop",      1 ],   # 08
-  [ "dad     b", 1 ],   # 09
-  [ "ldax    b", 1 ],   # 0A
-  [ "dcx     b", 1 ],   # 0B
-  [ "inr     c", 1 ],   # 0C
-  [ "dcr     c", 1 ],   # 0D
-  [ "mvi     c,", 2 ],  # 0E
-  [ "rrc",        1 ],  # 0F
+  [ "ld      bc,", 3 ],  # 01
+  [ "ld      (bc),a", 1 ], # 02
+  [ "inc     bc", 1 ],   # 03
+  [ "inc     b", 1 ],   # 04
+  [ "dec     b", 1 ],   # 05
+  [ "ld      b,", 2 ],  # 06
+  [ "rlca",      1 ],   # 07
+  [ "ex      af,af", 1 ], # 08
+  [ "add     hl,bc", 1 ], # 09
+  [ "ld      a,(bc)", 1 ], # 0A
+  [ "dec     bc", 1 ],   # 0B
+  [ "inc     c", 1 ],   # 0C
+  [ "dec     c", 1 ],   # 0D
+  [ "ld      c,", 2 ],  # 0E
+  [ "rrca",       1 ],  # 0F
 
-  [ "*nop", 1 ], # 10
-  [ "lxi     d,", 3 ],  # 11
-  [ "stax    d", 1 ],   # 12
-  [ "inx     d", 1 ],   # 13
-  [ "inr     d", 1 ],   # 14
-  [ "dcr     d", 1 ],   # 15
-  [ "mvi     d,", 2 ],  # 16
-  [ "ral", 1 ],         # 17
-  [ "*nop", 1 ],        # 18
-  [ "dad", 1 ],         # 19
-  [ "ldax    d", 1 ],   # 1A
-  [ "dcx     d", 1 ],   # 1B
-  [ "inr     e", 1 ],   # 1C
-  [ "dcr     e", 1 ],   # 1D
-  [ "mvi     e,", 2 ],  # 1E
-  [ "rar", 1 ],         # 1F
+  [ "djnz    ", 1 ], # 10
+  [ "ld      de,", 3 ],  # 11
+  [ "ld      (de),a,", 1 ],  # 12
+  [ "inc     de", 1 ],  # 13
+  [ "inc     d", 1 ],   # 14
+  [ "dec     d", 1 ],   # 15
+  [ "ld      d,", 2 ],  # 16
+  [ "rla", 1 ],         # 17
+  [ "jr      ", 1 ],    # 18
+  [ "add     hl,de", 1 ], # 19
+  [ "ld      a,(de)", 1 ],  # 1A
+  [ "dec     de", 1 ],   # 1B
+  [ "inc     e", 1 ],   # 1C
+  [ "dec     e", 1 ],   # 1D
+  [ "ld      e,", 2 ],  # 1E
+  [ "rra", 1 ],         # 1F
 
-  [ "*nop", 1 ],        # 20
-  [ "lxi     h,", 3 ],  # 21
-  [ "shld    ", 3 ],    # 22
-  [ "inx     h", 1 ],   # 23
-  [ "inr     h", 1 ],   # 24
-  [ "dcr     h", 1 ],   # 25
-  [ "mvi     h,", 2 ],  # 26
+  [ "jr      nz,", 1 ], # 20
+  [ "ld      hl,", 3 ],  # 21
+  [ "ld      (", 3 ],    # 22
+  [ "inc     hl", 1 ],   # 23
+  [ "inc     h", 1 ],   # 24
+  [ "dec     h", 1 ],   # 25
+  [ "ld      h,", 2 ],  # 26
   [ "daa", 1 ],         # 27
-  [ "*nop", 1 ],        # 28
-  [ "dad     h", 1 ],   # 29
-  [ "lhld    ", 3 ],    # 2A
-  [ "dcx     h", 1 ],   # 2B
-  [ "inr     l", 1 ],   # 2C
-  [ "dcr     l", 1 ],   # 2D
-  [ "mvi     l,", 2 ],  # 2E
-  [ "cma", 1 ],         # 2F
+  [ "jr      z,", 1 ],        # 28
+  [ "add     hl,jl", 1 ],   # 29
+  [ "ld      hl,(", 3 ],    # 2A
+  [ "dec     hl", 1 ],   # 2B
+  [ "inc     l", 1 ],   # 2C
+  [ "dec     l", 1 ],   # 2D
+  [ "ld      l,", 2 ],  # 2E
+  [ "cpl", 1 ],         # 2F
 
-  [ "*nop", 1 ],        # 30
-  [ "lxi     sp,", 3 ], # 31
-  [ "sta     ", 3 ],    # 32
-  [ "inx     sp", 1 ],  # 33
-  [ "inr     m", 1 ],   # 34
-  [ "dcr     m", 1 ],   # 35
-  [ "mvi     m,", 2 ],  # 36
-  [ "stc", 1 ],         # 37
-  [ "*nop", 1 ],        # 38
-  [ "dad     sp", 1 ],  # 39
-  [ "lda     ", 3 ],    # 3A
-  [ "dcx     sp", 1 ],  # 3B
-  [ "inr     a", 1 ],   # 3C
-  [ "dcr     a", 1 ],   # 3D
-  [ "mvi     a,", 2 ],  # 3E
-  [ "cmc", 1 ],         # 3F
+  [ "j       nc,", 1 ],        # 30
+  [ "ld      sp,", 3 ], # 31
+  [ "ld      (", 3 ],    # 32
+  [ "inc     sp", 1 ],  # 33
+  [ "inc     (hl)", 1 ],   # 34
+  [ "dec     (hl)", 1 ],   # 35
+  [ "ld      (hl),", 2 ],  # 36
+  [ "scf", 1 ],         # 37
+  [ "jr      c,", 1 ],        # 38
+  [ "add     hl,sp", 1 ],  # 39
+  [ "ld      a,(", 3 ],    # 3A
+  [ "dec     sp", 1 ],  # 3B
+  [ "inc     a", 1 ],   # 3C
+  [ "dec     a", 1 ],   # 3D
+  [ "ld      a,", 2 ],  # 3E
+  [ "ccf", 1 ],         # 3F
 
-  [ "mov     b,b", 1 ], # 40
-  [ "mov     b,c", 1 ], # 41
-  [ "mov     b,d", 1 ], # 42
-  [ "mov     b,e", 1 ], # 43
-  [ "mov     b,h", 1 ], # 44
-  [ "mov     b,l", 1 ], # 45
-  [ "mov     b,m", 1 ], # 46
-  [ "mov     b,a", 1 ], # 47
-  [ "mov     c,b", 1 ], # 48
-  [ "mov     c,c", 1 ], # 49
-  [ "mov     c,d", 1 ], # 4A
-  [ "mov     c,e", 1 ], # 4B
-  [ "mov     c,h", 1 ], # 4C
-  [ "mov     c,l", 1 ], # 4D
-  [ "mov     c,m", 1 ], # 4E
-  [ "mov     c,a", 1 ], # 4F
+  [ "ld      b,b", 1 ], # 40
+  [ "ld      b,c", 1 ], # 41
+  [ "ld      b,d", 1 ], # 42
+  [ "ld      b,e", 1 ], # 43
+  [ "ld      b,h", 1 ], # 44
+  [ "ld      b,l", 1 ], # 45
+  [ "ld      b,m", 1 ], # 46
+  [ "ld      b,a", 1 ], # 47
+  [ "ld      c,b", 1 ], # 48
+  [ "ld      c,c", 1 ], # 49
+  [ "ld      c,d", 1 ], # 4A
+  [ "ld      c,e", 1 ], # 4B
+  [ "ld      c,h", 1 ], # 4C
+  [ "ld      c,l", 1 ], # 4D
+  [ "ld      c,m", 1 ], # 4E
+  [ "ld      c,a", 1 ], # 4F
 
-  [ "mov     d,b", 1 ], # 50
-  [ "mov     d,c", 1 ], # 51
-  [ "mov     d,d", 1 ], # 52
-  [ "mov     d,e", 1 ], # 53
-  [ "mov     d,h", 1 ], # 54
-  [ "mov     d,l", 1 ], # 55
-  [ "mov     d,m", 1 ], # 56
-  [ "mov     d,a", 1 ], # 57
-  [ "mov     e,b", 1 ], # 58
-  [ "mov     e,c", 1 ], # 59
-  [ "mov     e,d", 1 ], # 5A
-  [ "mov     e,e", 1 ], # 5B
-  [ "mov     e,e", 1 ], # 5C
-  [ "mov     e,h", 1 ], # 5D
-  [ "mov     e,m", 1 ], # 5E
-  [ "mov     e,a", 1 ], # 5F
+  [ "ld      d,b", 1 ], # 50
+  [ "ld      d,c", 1 ], # 51
+  [ "ld      d,d", 1 ], # 52
+  [ "ld      d,e", 1 ], # 53
+  [ "ld      d,h", 1 ], # 54
+  [ "ld      d,l", 1 ], # 55
+  [ "ld      d,m", 1 ], # 56
+  [ "ld      d,a", 1 ], # 57
+  [ "ld      e,b", 1 ], # 58
+  [ "ld      e,c", 1 ], # 59
+  [ "ld      e,d", 1 ], # 5A
+  [ "ld      e,e", 1 ], # 5B
+  [ "ld      e,e", 1 ], # 5C
+  [ "ld      e,h", 1 ], # 5D
+  [ "ld      e,m", 1 ], # 5E
+  [ "ld      e,a", 1 ], # 5F
 
-  [ "mov     h,b", 1 ], # 60
-  [ "mov     h,c", 1 ], # 61
-  [ "mov     h,d", 1 ], # 62
-  [ "mov     h,e", 1 ], # 63
-  [ "mov     h,h", 1 ], # 64
-  [ "mov     h,l", 1 ], # 65
-  [ "mov     h,m", 1 ], # 66
-  [ "mov     h,a", 1 ], # 67
-  [ "mov     l,b", 1 ], # 68
-  [ "mov     l,c", 1 ], # 69
-  [ "mov     l,d", 1 ], # 6A
-  [ "mov     l,e", 1 ], # 6B
-  [ "mov     l,h", 1 ], # 6C
-  [ "mov     l,l", 1 ], # 6D
-  [ "mov     l,m", 1 ], # 6E
-  [ "mov     l,a", 1 ], # 6F
+  [ "ld      h,b", 1 ], # 60
+  [ "ld      h,c", 1 ], # 61
+  [ "ld      h,d", 1 ], # 62
+  [ "ld      h,e", 1 ], # 63
+  [ "ld      h,h", 1 ], # 64
+  [ "ld      h,l", 1 ], # 65
+  [ "ld      h,m", 1 ], # 66
+  [ "ld      h,a", 1 ], # 67
+  [ "ld      l,b", 1 ], # 68
+  [ "ld      l,c", 1 ], # 69
+  [ "ld      l,d", 1 ], # 6A
+  [ "ld      l,e", 1 ], # 6B
+  [ "ld      l,h", 1 ], # 6C
+  [ "ld      l,l", 1 ], # 6D
+  [ "ld      l,m", 1 ], # 6E
+  [ "ld      l,a", 1 ], # 6F
 
-  [ "mov     m,b", 1 ], # 70
-  [ "mov     m,c", 1 ], # 71
-  [ "mov     m,d", 1 ], # 72
-  [ "mov     m,e", 1 ], # 73
-  [ "mov     m,h", 1 ], # 74
-  [ "mov     m,l", 1 ], # 75
+  [ "ld      m,b", 1 ], # 70
+  [ "ld      m,c", 1 ], # 71
+  [ "ld      m,d", 1 ], # 72
+  [ "ld      m,e", 1 ], # 73
+  [ "ld      m,h", 1 ], # 74
+  [ "ld      m,l", 1 ], # 75
   [ "hlt",         1 ], # 76
-  [ "mov     m,a", 1 ], # 77
-  [ "mov     a,b", 1 ], # 78
-  [ "mov     a,c", 1 ], # 79
-  [ "mov     a,d", 1 ], # 7A
-  [ "mov     a,e", 1 ], # 7B
-  [ "mov     a,h", 1 ], # 7C
-  [ "mov     a,l", 1 ], # 7D
-  [ "mov     a,m", 1 ], # 7E
-  [ "mov     a,a", 1 ], # 7F
+  [ "ld      (hl),a", 1 ], # 77
+  [ "ld      a,b", 1 ], # 78
+  [ "ld      a,c", 1 ], # 79
+  [ "ld      a,d", 1 ], # 7A
+  [ "ld      a,e", 1 ], # 7B
+  [ "ld      a,h", 1 ], # 7C
+  [ "ld      a,l", 1 ], # 7D
+  [ "ld      a,m", 1 ], # 7E
+  [ "ld      a,a", 1 ], # 7F
 
   [ "add     b", 1 ],   # 80
   [ "add     c", 1 ],   # 81
@@ -217,7 +217,7 @@ lookupTable = [
   [ "xra     a", 1 ],   # AF
 
   [ "ora     b", 1 ],   # B0
-  [ "ora     c", 1 ],   # B1
+  [ "or      c", 1 ],   # B1
   [ "ora     d", 1 ],   # B2
   [ "ora     e", 1 ],   # B3
   [ "ora     h", 1 ],   # B4
@@ -236,15 +236,15 @@ lookupTable = [
   [ "rnz", 1 ],         # C0
   [ "pop     b", 1 ],   # C1
   [ "jnz     ", 3 ],    # C2
-  [ "jmp     ", 3 ],    # C3
+  [ "jp      ", 3 ],    # C3
   [ "cnz     ", 3 ],    # C4
   [ "push    b", 1 ],   # C5
   [ "adi     ", 2 ],    # C6
   [ "rst     0", 1 ],   # C7
-  [ "rz", 1 ],          # C8
+  [ "ret     z", 1 ],          # C8
   [ "ret", 1 ],         # C9
   [ "jz      ", 3 ],    # CA
-  [ "*jmp     ", 3 ],   # CB
+  [ "table", 3 ],   # CB
   [ "cz      ", 3 ],    # CC
   [ "call    ", 3 ],    # CD
   [ "aci     ", 2 ],    # CE
@@ -263,7 +263,7 @@ lookupTable = [
   [ "jc      ", 3 ],    # DA
   [ "in      ", 2 ],    # DB
   [ "cc      ", 3 ],    # DC
-  [ "*call    ", 3 ],   # DD
+  [ "table", 3 ],   # DD
   [ "sbi     ", 2 ],    # DE
   [ "rst     3", 1 ],   # DF
 
@@ -280,7 +280,7 @@ lookupTable = [
   [ "jpe     ", 3 ],    # EA
   [ "xchg", 1 ],        # EB
   [ "cpe     ", 3 ],    # EC
-  [ "*call    ", 3 ],   # ED
+  [ "table", 3 ],   # ED
   [ "xri     ", 2 ],    # EE
   [ "rst     5", 1 ],   # EF
 
@@ -297,9 +297,9 @@ lookupTable = [
   [ "jm      ", 3 ],    # FA
   [ "ei", 1 ],          # FB
   [ "cm      ", 3 ],    # FC
-  [ "*call    ", 3 ],   # FD
+  [ "table", 3 ],   # FD
   [ "cpi     ", 2 ],    # FE
-  [ "rst     7", 1 ],   # FF
+  [ "rst     38", 1 ],   # FF
 ]
 
 upperOption = False
