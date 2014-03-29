63e13af42fffe579dd6c4e3befc5f0b2e87f997f
Jeff Tranter <tranter@pobox.com>
2014-03-29 15:14:11 -0400

    Implement more functionality.

--- asm/jmon/jmon.asm
+++ asm/jmon/jmon.asm
@@ -24,8 +24,20 @@
 ;   DUMP: D <START>
 ;   GO: G <ADDRESS>
 ;   CLR SCREEN: L
+;   INFO: N
 ;   REGISTERS: R
 ;   HELP: ?
+;
+; Possible future commands:
+;
+; COPY: C <START> <END> <DEST>
+; FILL: F <START> <END> <DATA>...
+; CHECKSUM: K <START> <END>
+; SEARCH: S <START> <END> <DATA>...
+; TEST: T <START> <END>
+; VERIFY: V <START> <END> <DEST>
+; WRITE: : <ADDRESS> <DATA>...
+; MATH: = <ADDRESS> +/- <ADDRESS>
 
         cpu     8080
         org     0000H   ; Use 0100H if you want to run under CP/M
@@ -35,7 +47,7 @@
 prompt  equ '?'          ; Prompt character
 CR      equ '\r'         ; Carriage Return
 NL      equ '\n'         ; Newline
-stack   equ 8000h        ; Starting address for stack
+stack   equ 7000h        ; Starting address for stack
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
@@ -43,9 +55,7 @@ stack   equ 8000h        ; Starting address for stack
  
 start:
         lxi     sp,stack        ; Set up stack pointer
-
         call    ClearScreen     ; Clear screen
-
         lxi     h,strStartup    ; Print startup message
         call    PrintString
 
@@ -54,42 +64,44 @@ mainloop:
         call    PrintChar
 
         call    GetChar         ; Get a command (letter)
+        call    ToUpper         ; Convert to upper case
 
         cpi     'D'             ; DUMP command?
         jnz     tryG
-        call    PrintChar       ; Echo it back
         call    DumpCommand
         jmp     mainloop
 
 tryG:
         cpi     'G'             ; GO command?
         jnz     tryL
-        call    PrintChar       ; Echo it back
         call    GoCommand
         jmp     mainloop
 
 tryL:
         cpi     'L'             ; CLEAR command?
-        jnz     tryR
-        call    PrintChar       ; Echo it back
+        jnz     tryN
         call    ClearCommand
         jmp     mainloop
+
+tryN:   cpi     'N'             ; INFO command?
+        jnz     tryR
+        call    InfoCommand
+        jmp     mainLoop
 
 tryR:
         cpi     'R'             ; REGISTERS command?
         jnz     tryHelp
-        call    PrintChar       ; Echo it back
         call    RegistersCommand
         jmp     mainloop
 
 tryHelp:
         cpi     '?'             ; HELP command?
         jnz     invalid
-        call    PrintChar       ; Echo it back
         call    HelpCommand
         jmp     mainloop
 
 invalid:
+        call    PrintCR
         lxi     h,strInvalid    ; print error message
         call    PrintString
 
@@ -100,22 +112,66 @@ invalid:
 ; Commands
 
 DumpCommand:
+        call    PrintChar       ; Echo command back
         call    PrintCR
         ret
 
 GoCommand:
+        call    PrintChar       ; Echo command back
         call    PrintCR
         ret
 
+; CLEAR command.
+; Sends code to clear terminal screen.
+
 ClearCommand:
+        call    PrintChar       ; Echo command back
         call    ClearScreen     ; Clear screen
         ret
 
+
+; INFO command.
+InfoCommand:
+        call    PrintChar       ; Echo command back
+        call    PrintCR
+        lxi     h,strStartup    ; Print startup message
+        call    PrintString
+
+; Detect CPU type. 8080 flags have XX0X0X1X. If different, then assume Z80.
+
+        lxi     h,strCpuType
+        call    PrintString
+
+        push    psw             ; Push A and flags
+        pop     b               ; Pop B and C, flags now in C
+        mov     a,c             ; Move flags to A
+        ani     00101010b       ; Mask out bit we don't care about
+        cpi     00000010b       ; Should have this value for 8080
+        jnz     z80             ; If not, assume Z80 CPU
+        lxi     h,str8080       ; It is an 8080
+prnt:   call    PrintString     ; Print CPU type
+        call    PrintCR
+        ret                     ; Return
+z80:    
+        lxi     h,strZ80        ; It is a Z80
+        jmp     prnt
+
+
+; REGISTERS command.
+; Example output:
+; A=01 BC=4E56 DE=0000 HL=021C .Z-.-P-C SP=6FFE PC=00C3
+
 RegistersCommand:
+        call    PrintChar       ; Echo command back
         call    PrintCR
         ret
 
+
+; HELP command.
+; Displays list of valid commands.
+
 HelpCommand:
+        call    PrintChar       ; Echo command back
         lxi     h,strHelp
         call    PrintString
         ret
@@ -159,14 +215,14 @@ DREG    port    11h
 ; Registers affected: none.
 
 PrintChar:
-        push    psw     ; Save A register
-loop1:  in      SREG    ; Read status register
-        rrc             ; Move TDRE to carry bit
+        push    psw             ; Save A register
+loop1:  in      SREG            ; Read status register
+        rrc                     ; Move TDRE to carry bit
         rrc
-        jnc     loop1   ; Repeat until TDRE is set
-        pop     psw     ; Restore A
-        out     DREG    ; Output it to data register
-        ret             ; And return
+        jnc     loop1           ; Repeat until TDRE is set
+        pop     psw             ; Restore A
+        out     DREG            ; Output it to data register
+        ret                     ; And return
 
 ; GetChar
 ; Read character from console and return in A register. The character
@@ -174,23 +230,23 @@ loop1:  in      SREG    ; Read status register
 ; Registers affected: A.
 
 GetChar:
-        in      SREG    ; Read status register
-        rrc             ; Move RDRF to carry bit
-        jnc     GetChar ; Repeat until RDRF is set
-        in      DREG    ; Read character from data register
-        ret             ; And return
+        in      SREG            ; Read status register
+        rrc                     ; Move RDRF to carry bit
+        jnc     GetChar         ; Repeat until RDRF is set
+        in      DREG            ; Read character from data register
+        ret                     ; And return
 
 ; PrintCR
 ; Print carriage return/newline.
 ; Registers affected: none
 
 PrintCR:
-        push    psw
-        mvi     a,CR
+        push    psw             ; Save A reg
+        mvi     a,CR            ; Carriage Return character
+        call    PrintChar       
+        mvi     a,NL            ; Newline character
         call    PrintChar
-        mvi     a,NL
-        call    PrintChar
-        pop     psw
+        pop     psw             ; Restore A reg
         ret
 
 ; ClearScreen
@@ -217,13 +273,47 @@ nextch: mov     a,m             ; Get a character
 eos:    pop     psw             ; Restore A register
         ret                     ; Return
 
+; PrintSpace
+; Print space character.
+; Registers affected: none
+PrintSpace:
+        push    psw             ; Save A reg
+        mvi     a,' '           ; Space character
+        call    PrintChar       
+        pop     psw             ; Restore A reg
+        ret
+
+; ToUpper
+; Convert character in A to uppercase if it is a letter.
+; Registers affected: A
+ToUpper:
+        cpi     'a'             ; Less than 'a' ?
+        jc      notUpper        ; If so, branch
+        cpi     'z'+1           ; Greater than 'z'?
+        jnc     notUpper
+        ani     11011111b       ; Convert to upper case
+notUpper:
+        ret
+
+PrintByte:
+
+PrintAddress:
+
+GetHex:
+
+GetByte:
+
+GetAddress:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
 ; Strings
 
 strStartup:
         db      "JMON Monitor 0.1 by Jeff Tranter\r\n",0
 
 strInvalid:
-        db      "Invalid command",0
+        db      "Invalid command. Type ? for help.",0
 
 strHelp:
         db      "\r\n"
@@ -231,10 +321,18 @@ strHelp:
         db      "D <address>      Dump memory\r\n"
         db      "G <address>      Go\r\n"
         db      "L                Clear screen\r\n"
+        db      "N                Show Info\r\n"
         db      "R                Show registers\r\n"
         db      "?                Help\r\n",0
 
 strClearScreen:
         db      "\e[2J\e[H",0       ; VT100/ANSI clear screen, cursor home
+
+strCpuType:
+        db      "CPU type: ",0
+str8080:
+        db      "8080",0
+strZ80:
+        db      "Z80",0
 
         end
