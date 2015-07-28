d72a8f6e2d5ad3f399fd682b70d0accfecd05294
Jeff Tranter <tranter@pobox.com>
2015-07-27 22:18:33 -0400

    Enter mode code.

--- asm/zmon/zmon.asm
+++ asm/zmon/zmon.asm
@@ -6,60 +6,154 @@ STACK   EQU     0E26EH
 ;       ZMON 6.2
 ;
 ;
-        JMP     INIT    ;0F021H
-        JMP     CONIN   ;0F389H
-        JMP     CONOUT  ;0F37EH
-        JMP     PRINT   ;0F1A1H
-        JMP     CONSTAT ;0F3ADH
-        JMP     INIT    ;0F021H
+        JMP     INIT
+        JMP     CONIN
+        JMP     CONOUT
+        JMP     PRINT
+        JMP     CONSTAT
+        JMP     INIT
 ;
 ;       COLD START MONITOR
 ;
-INIT:           ;F021
-        LXI     SP,STACK        ;0E26EH
-        CALL    CLRSCR  ;0F3A4H
+INIT:
+        LXI     SP,STACK
+        CALL    CLRSCR
         PUSH    H
-        LXI     H,SOMSG ;0F3D5H
-        CALL    OSTR    ;0F394H
+        LXI     H,SOMSG
+        CALL    OSTR
         POP     H
-        CALL    PINIT   ;0F18AH
+        CALL    PINIT
 ;
 ;GET A COMMAND AND PARSE IT
 ;
 PARSE:  IN      1
-        CALL    PROMPT  ;0F348H
-        CALL    CMD.    ;0F36BH
-        CPI     'E'     ;45H
-        JZ      EXAM    ;0F070H
-        CPI     'M'     ;4DH
-        JZ      DUMP    ;0F146H
+        CALL    PROMPT
+        CALL    CMD.
+        CPI     'E'
+        JZ      EXAM
+        CPI     'M'
+        JZ      DUMP
 ;
 ;       ERROR IN COMMAND
 ;
-ERROR:          ;F067
-        MVI     A,'?'   ;3FH
-        CALL    PNT     ;0F373H
-        JMP     PARSE   ;0F032H
+ERROR:
+        MVI     A,'?'
+        CALL    PNT
+        JMP     PARSE
 ;
 ;       EXAMINE MEMORY
 ;
-EXAM:           ;F070
-        CALL    EPROMPT ;0F353H
-        CALL    LOADHL  ;0F1F0H
-EXMN:           ;F076
-        CALL    LFCR    ;0F35EH
-        CALL    POH     ;0F1FBH
-        MVI     A,':'   ;3AH
-        CALL    PNT     ;0F373H
-        CALL    CMD     ;0F36BH
-        CPI     ' '     ;20H
-        JZ      NEXT    ;0F096H
-        CPI     'R'     ;52H
-        JZ      RUN     ;0F09AH
+EXAM:
+        CALL    EPROMPT
+        CALL    LOADHL
+EXMN:
+        CALL    LFCR
+        CALL    POH
+        MVI     A,':'
+        CALL    PNT
+        CALL    CMD
+        CPI     ' '
+        JZ      NEXT
+        CPI     'R'
+        JZ      RUN
         CPI     0DH
-        JZ      PARSE   ;0F032H
-        JMP     DEPOS   ;0F09BH
+        JZ      PARSE
+        JMP     DEPOS
 ;
 ;       EXAMINE NEXT
 ;
 NEXT:
+        INX     H
+        JMP     EXMN
+;
+;       RUN
+;
+RUN:
+        PCHL
+;
+;       DEPOSIT
+;
+DEPOS:
+        CALL    MORE
+        MOV     M,A
+        CALL    SPACE
+        MOV     A,M
+        CALL    PHCHAR
+        INX     H
+        JMP     EXAM
+;
+;
+;       DUMP MEMORY
+;
+DUMP:
+        CALL    EPROMPT
+        CALL    BOUND
+        CALL    CLRSCR
+D0:     MVI     B,18H   ;SETB FOR 24 LINES ON A SCREEN
+D1:     CALL    LFCR
+        MVI     C,10H   ;SET C FOR 16 BYTES ACROSS
+;
+;       PRINT ADDRESS STARTING THIS LINE
+;
+        MOV     A,H
+        CALL    PHCHAR
+        MOV     A,L
+        CALL    PHCHAR
+;
+        CALL    SPACE
+PNTBYTE:
+        MOV     A,M     ;GET THE BYTE FROM MEMORY
+        CALL    PHCHAR  ;AND PRINT IT
+        CALL    SPACE   ;AND A SPACE
+;
+;       ARE WE DONE
+;
+        MOV     A,H
+        CMP     D
+        JNZ     KEEPON  ;NOT DONE
+        MOV     A,L
+        CMP     E
+        JZ      PARSE   ;DONE
+;
+KEEPON: INX     H
+        DCR     C
+        JZ      LINEDONE        ;THIS LINE DONE
+;
+;       IS HE BEATING ON THE KEYBOARD?
+;
+        IN      0
+        RRC
+        JNC     PARSE
+;
+        JMP     PNTBYTE
+;
+LINEDONE:
+        DCR     B
+        JNZ     D1      ;
+        CALL    CMD
+        MVI     B,10H
+        JMP     D0
+;
+;       INIT PRINTER
+;
+PINIT:
+        MVI     A,0
+        OUT     0C0H    ;SET UP
+        OUT     0C2H    ;DDRS
+        OUT     0C1H    ;INPUT
+        CMA
+        OUT     0C3H    ;OUTPUT
+        MVI     A,"="
+        OUT     0C0H
+        MVI     A,"="
+        OUT     0C2H
+        CALL    STAT
+        RET
+;
+;       PRINT A CHARACTER
+;
+PRINT:
+        PUSH    B
+        MVI     B,64H   ;RETRY COUNT
+PNT1:   DCR     B
+
