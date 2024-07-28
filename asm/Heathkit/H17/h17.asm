bd8c28a1a766fb25719dcae97562a4bdc11875e1
Jeff Tranter <tranter@pobox.com>
2024-07-28 08:50:55 -0400

    Entering code for H17 floppy ROM.

--- asm/Heathkit/H17/h17.asm
+++ asm/Heathkit/H17/h17.asm
@@ -30,14 +30,6 @@ ERPTCNT EQU     10              ; SOFT ERROR RETRY COUNT
         INCLUDE esval.asm
         INCLUDE esint.asm
 
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
         ORG     14000Q
 
         JMP     BOOT            ; BOOT CODE
@@ -50,4 +42,37 @@ ERPTCNT EQU     10              ; SOFT ERROR RETRY COUNT
         XCHG                    ; (DE) = END+1
         LXI     H,20100Q        ; (HL) = START
         HLT                     ; PAUSE FOR ADJUSTMENT
+
+;       (HL) = START
+;       (DE) = END
+
+;       ZERO TEST AREA
+
+        SHLD    20100Q-2
+MEM1    MVI     M,0
+        INX     H
+        CALL    DCDEHL
+        JNZ     MEM1
+
+;       START TESTING MEMORY. INCREMENT EACH BYTE IN TURN, AND COMPARE
+;       THAT RESULT TO THE EXPECTED VALUE
+
+        MVI     B,0             ; (B) = EXPECTED VALUE
+MEM2    LHLD    20100Q-2
+        INR     B
+
+MEM3    INR     M
+        MOV     A,M             ; (A) = VALUE
+        CMP     B
+        JZ      MEM4            ; IS OK
+
+;       HAVE ERROR. (HL) = ADDRESS OF BYTE IN ERROR
+
+        HLT
+        NOP
+
+MEM4    INX     H
+        CALL    DCDEHL
+        JNZ     MEM3            ; NOT AT END OF PASS
+        JMP     MEM2            ; AT END OF PASS
 
